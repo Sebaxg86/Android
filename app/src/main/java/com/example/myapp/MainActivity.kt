2f3f9cdeb56cc531f43902234f979fcbb36c9aa4
Sebastian Chairez <sebax86@icloud.com>
2026-09-03 14:01:35 -0600

    Contador completado

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,17 +44,38 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun Formulario(){
-    Box(
-        modifier=Modifier
-            .padding(150.dp)
-            .size(150.dp)
-            .background(
-                color=Color.Yellow,
-                shape = CircleShape
-            )
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ){
-        Text("Hi",
-                fontSize = 70.sp,
-                color = Color.White)
+        Box(
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .size(150.dp)
+                .background(color=Color.Blue,
+                            shape = CircleShape),
+            contentAlignment = Alignment.Center
+
+        ){
+            Text("0",
+                    fontSize = 70.sp,
+                    color=Color.White)
+        }
+
+        Button(
+            onClick = {
+
+            },
+            modifier = Modifier
+                .width(250.dp)
+        ){
+            Text("Sumar",
+                fontSize = 30.sp)
+        }
     }
+
 }
+
