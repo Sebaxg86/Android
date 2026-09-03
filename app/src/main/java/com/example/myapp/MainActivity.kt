cfc32e1c652da866d7cd8e5ae68e0f9a47192417
Sebastian Chairez <sebax86@icloud.com>
2026-09-03 13:11:56 -0600

    update

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -4,23 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapp.ui.theme.MyAppTheme
+import java.nio.file.WatchEvent
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +38,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Formulario(modifier = Modifier.padding(innerPadding))
+                    Formulario()
                 }
             }
         }
@@ -37,18 +46,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Formulario(modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(96.dp),
-        horizontalArrangement = Arrangement.spacedBy(12.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text("Titulo", fontSize = 16.sp)
-        Text("Subtitulo", fontSize = 14.sp)
-        Button(onClick = {}) {
-            Text("Ingresar", fontWeight = FontWeight.Bold)
-        }
+fun Formulario(){
+    LazyColumn(
+        contentPadding = PaddingValues(12.dp)
+    ){
+        Texto("Hola", Color.Red)
+        Espacio(10)
+        Texto("Jetpack", Color.Cyan)
+        Espacio(10)
+        Texto("Compose", Color.Green)
     }
+}
+
+@Composable
+fun Espacio(espacio: Int){
+    Spacer(modifier=Modifier.padding(espacio.dp))
+}
+
+@Composable
+fun Texto(texto:String, color: Color){
+    Text(
+        text=texto,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+        color= color,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(Color.Black)
+            .fillMaxWidth()
+    )
 }
