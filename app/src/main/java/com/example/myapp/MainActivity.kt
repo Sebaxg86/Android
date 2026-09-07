91efe26451fd72c876987b29a050105b9ec1249e
Sebastian Chairez <sebax86@icloud.com>
2026-09-07 13:52:32 -0600

    chore: Práctica 2 Terminada

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,6 +21,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -44,35 +50,146 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun Formulario(){
+
+    //Variables de los contadores
+    var contadorLocal by remember { mutableStateOf(0) }
+    var contadorVisitante by remember { mutableStateOf(0) }
+
+    //Columna que contiene todo
     Column(
         modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
-        Box(
+        //Fila que contenga las dos columnas
+        Row(
             modifier = Modifier
-                .padding(bottom = 30.dp)
-                .size(150.dp)
-                .background(color=Color.Blue,
-                            shape = CircleShape),
-            contentAlignment = Alignment.Center
-
+                .weight(1f)
+                .padding(5.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ){
-            Text("0",
-                    fontSize = 70.sp,
-                    color=Color.White)
+            //Columna del equipo local
+            Column(
+                modifier = Modifier
+                    .weight(1f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ){
+                //Caja del contador local
+                Box(
+                    modifier = Modifier
+                        .padding(bottom = 30.dp)
+                        .size(150.dp)
+                        .background(color=Color.Blue,
+                            shape = CircleShape),
+                    contentAlignment = Alignment.Center
+
+                ){
+                    Text(text="$contadorLocal",
+                        fontSize = 70.sp,
+                        color=Color.White)
+                }
+
+
+
+                //Botón de sumar
+                Button(
+                    onClick = {
+                        contadorLocal++
+                    },
+                    modifier = Modifier
+                        .width(250.dp)
+                ){
+                    Text("Sumar",
+                        fontSize = 30.sp)
+                }
+
+                Spacer(modifier= Modifier.padding(8.dp))
+
+                //Botón de restar
+                Button(
+                    onClick = {
+                        if (contadorLocal>0){
+                            contadorLocal--
+                        }
+
+                    },
+                    modifier = Modifier
+                        .width(250.dp)
+                ){
+                    Text("Restar",
+                        fontSize = 30.sp)
+                }
+            }
+
+            //Columna del equipo visitante
+            Column(
+                modifier = Modifier
+                    .weight(1f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ){
+                //Caja con el contador
+                Box(
+                    modifier = Modifier
+                        .padding(bottom = 30.dp)
+                        .size(150.dp)
+                        .background(color=Color.Blue,
+                            shape = CircleShape),
+                    contentAlignment = Alignment.Center
+
+                ){
+                    Text(text="$contadorVisitante",
+                        fontSize = 70.sp,
+                        color=Color.White)
+                }
+
+                //Botón de Sumar
+                Button(
+                    onClick = {
+                        contadorVisitante++
+                    },
+                    modifier = Modifier
+                        .width(250.dp)
+                ){
+                    Text("Sumar",
+                        fontSize = 30.sp)
+                }
+
+                Spacer(modifier= Modifier.padding(8.dp))
+
+                //Botón de Restar
+                Button(
+                    onClick = {
+                        if(contadorVisitante>0){
+                            contadorVisitante--
+                        }
+                    },
+                    modifier = Modifier
+                        .width(250.dp)
+                ){
+                    Text("Restar",
+                        fontSize = 30.sp)
+                }
+            }
         }
 
+        //Botón de Reiniciar
         Button(
             onClick = {
-
+                contadorVisitante=0
+                contadorLocal=0
             },
             modifier = Modifier
                 .width(250.dp)
+                .padding(bottom = 50.dp)
         ){
-            Text("Sumar",
+            Text("Reiniciar",
                 fontSize = 30.sp)
         }
     }
